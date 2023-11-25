a2d4281d241f625a75b1e0c8fd5d2c9366328595
Jeff Pursell <jpursell@gmail.com>
2023-11-25 09:24:39 -0500

    Refactor out duplicates from 130::mod.rs

--- src/v1_3_0/mod.rs
+++ src/v1_3_0/mod.rs
@@ -1,7 +1,6 @@
 //! Common types and metadata definition for SICD Version 1.3.0 [2021-11-30]
 //!
 //! Backwards compatible with version 1, 1.1, 1.2.1
-use ndarray::{Array1, Array2};
 use serde::Deserialize;
 
 pub mod antenna;
@@ -20,6 +19,9 @@ pub mod radiometric;
 pub mod scpcoa;
 pub mod timeline;
 
+pub use crate::dep::v0_4_0::{
+    IdxLL, IdxLLH, IdxRowCol, IdxXyzPoly, Poly1D, Poly2D, RowCol, XyzPoly, CMPLX, LL, LLH, XYZ,
+};
 use antenna::Antenna;
 use collection_info::CollectionInfo;
 use error_statistics::ErrorStatistics;
@@ -72,129 +74,6 @@ pub struct SicdMeta {
     pub pfa: Option<Pfa>,
     #[serde(rename = "RMA")]
     pub rma: Option<Rma>,
-}
-
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct RowCol {
-    #[serde(rename = "Row")]
-    pub row: i64,
-    #[serde(rename = "Col")]
-    pub col: i64,
-}
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct IdxRowCol {
-    #[serde(rename = "@index")]
-    pub index: usize,
-    #[serde(rename = "Row")]
-    pub row: i64,
-    #[serde(rename = "Col")]
-    pub col: i64,
-}
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct CMPLX {
-    #[serde(rename = "Real")]
-    pub real: f64,
-    #[serde(rename = "Imag")]
-    pub imag: f64,
-}
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct XYZ {
-    #[serde(rename = "X")]
-    pub x: f64,
-    #[serde(rename = "Y")]
-    pub y: f64,
-    #[serde(rename = "Z")]
-    pub z: f64,
-}
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct LLH {
-    #[serde(rename = "Lat")]
-    pub lat: f64,
-    #[serde(rename = "Lon")]
-    pub lon: f64,
-    #[serde(rename = "HAE")]
-    pub hae: f64,
-}
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct IdxLLH {
-    #[serde(rename = "@index")]
-    pub index: usize,
-    #[serde(rename = "Lat")]
-    pub lat: f64,
-    #[serde(rename = "Lon")]
-    pub lon: f64,
-    #[serde(rename = "HAE")]
-    pub hae: f64,
-}
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct LL {
-    #[serde(rename = "Lat")]
-    pub lat: f64,
-    #[serde(rename = "Lon")]
-    pub lon: f64,
-}
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct IdxLL {
-    #[serde(rename = "@index")]
-    pub index: usize,
-    #[serde(rename = "Lat")]
-    pub lat: f64,
-    #[serde(rename = "Lon")]
-    pub lon: f64,
-}
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct Coef1D {
-    #[serde(rename = "@exponent1")]
-    pub exponent1: usize,
-    #[serde(rename = "$value")]
-    pub value: f64,
-}
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct Poly1D {
-    #[serde(rename = "@order1")]
-    pub order1: usize,
-    #[serde(rename = "$value")]
-    pub coefs: Vec<Coef1D>,
-}
-
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct Coef2D {
-    #[serde(rename = "@exponent1")]
-    pub exponent1: usize,
-    #[serde(rename = "@exponent2")]
-    pub exponent2: usize,
-    #[serde(rename = "$value")]
-    pub value: f64,
-}
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct Poly2D {
-    #[serde(rename = "@order1")]
-    pub order1: usize,
-    #[serde(rename = "@order2")]
-    pub order2: usize,
-    #[serde(rename = "$value")]
-    pub coefs: Vec<Coef2D>,
-}
-
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct XyzPoly {
-    #[serde(rename = "X")]
-    pub x: Poly1D,
-    #[serde(rename = "Y")]
-    pub y: Poly1D,
-    #[serde(rename = "Z")]
-    pub z: Poly1D,
-}
-#[derive(Debug, Deserialize, PartialEq, Clone)]
-pub struct IdxXyzPoly {
-    #[serde(rename = "@index")]
-    pub index: usize,
-    #[serde(rename = "X")]
-    pub x: Poly1D,
-    #[serde(rename = "Y")]
-    pub y: Poly1D,
-    #[serde(rename = "Z")]
-    pub z: Poly1D,
 }
 
 #[derive(Debug, Deserialize, PartialEq, Clone)]
@@ -384,55 +263,6 @@ pub enum DualPolarization {
     OTHER_OTHER,
     OTHER,
     UNKNOWN,
-}
-
-impl Poly1D {
-    /// Parse the data in the polynomial to an array object
-    pub fn to_array(&self) -> Array1<f64> {
-        let mut poly = Array1::zeros(self.order1 + 1);
-        for coef in &self.coefs {
-            let term = coef.exponent1;
-            poly[term] = coef.value;
-        }
-        poly
-    }
-
-    /// Evaluate the polynomial at a point
-    pub fn eval(&self, x: f64) -> f64 {
-        let mut res = 0f64;
-        for coef in &self.coefs {
-            res += coef.value * x.powi(coef.exponent1 as i32)
-        }
-        res
-    }
-}
-impl Poly2D {
-    /// Parse the data in the polynomial to an array object
-    pub fn to_array(&self) -> Array2<f64> {
-        let mut poly = Array2::zeros((self.order1 + 1, self.order2 + 1));
-        for coef in &self.coefs {
-            let term1 = coef.exponent1;
-            let term2 = coef.exponent2;
-            poly[[term1, term2]] = coef.value;
-        }
-        poly
-    }
-    /// Evaluate the polynomial at a point
-    pub fn eval(&self, x: f64, y: f64) -> f64 {
-        let mut res = 0f64;
-        for coef in &self.coefs {
-            res += coef.value * x.powi(coef.exponent1 as i32) * y.powi(coef.exponent2 as i32)
-        }
-        res
-    }
-}
-impl XyzPoly {
-    pub fn eval(&self, t: f64) -> Vec<f64> {
-        let x_pos = self.x.eval(t);
-        let y_pos = self.y.eval(t);
-        let z_pos = self.z.eval(t);
-        vec![x_pos, y_pos, z_pos]
-    }
 }
 
 #[cfg(test)]
