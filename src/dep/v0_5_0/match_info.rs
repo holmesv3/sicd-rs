6bacd1edc1dcf8b78db5beda4d27cc51fbec45ec
Jeff Pursell <jpursell@gmail.com>
2023-12-01 12:42:04 -0500

    Add match info test

--- src/dep/v0_5_0/match_info.rs
+++ src/dep/v0_5_0/match_info.rs
@@ -8,3 +8,38 @@ pub struct MatchInfo {
     #[serde(rename = "Collect")]
     pub collect: Vec<Collect>,
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_match_info() {
+        let xml = r#"
+          <MatchInfo size="1686">
+    <Collect index="-4879">
+      <CollectorName>string</CollectorName>
+      <IlluminatorName>string</IlluminatorName>
+      <CoreName>string</CoreName>
+      <MatchType>string</MatchType>
+      <MatchType>string</MatchType>
+      <Parameter name="string">string</Parameter>
+      <Parameter name="string">string</Parameter>
+      <Parameter name="string">string</Parameter>
+      <Parameter name="string">string</Parameter>
+    </Collect>
+    <Collect index="2689">
+      <CollectorName>string</CollectorName>
+      <CoreName>string</CoreName>
+      <MatchType>string</MatchType>
+      <Parameter name="string">string</Parameter>
+    </Collect>
+  </MatchInfo>
+        "#;
+        assert!(match quick_xml::de::from_str::<super::MatchInfo>(xml) {
+            Ok(_) => true,
+            Err(e) => {
+                dbg!(e);
+                false
+            }
+        });
+    }
+}
