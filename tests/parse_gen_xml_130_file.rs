4589d1a267391d9571c9c5d7d08120f91f029a3b
Jeff Pursell <jpursell@gmail.com>
2023-12-01 09:13:03 -0500

    Complete component test, add 050 test

--- tests/parse_gen_xml_130_file.rs
+++ tests/parse_gen_xml_130_file.rs
@@ -2,12 +2,11 @@ use quick_xml::de::from_str;
 use quick_xml::events::Event;
 use quick_xml::name::QName;
 use quick_xml::reader::Reader;
-use sicd_rs::v1_3_0::SicdMeta;
 
 #[test]
-fn test_generated_xml() {
+fn test_generated_130_xml() {
     let xml = include_str!("sicd_meta_130.xml");
-    let sicd_meta = from_str::<SicdMeta>(xml);
+    let sicd_meta = from_str::<sicd_rs::v1_3_0::SicdMeta>(xml);
     assert!(match sicd_meta {
         Ok(_) => true,
         Err(err) => {
@@ -18,7 +17,22 @@ fn test_generated_xml() {
 }
 
 #[test]
-fn parse_components_test() {
+fn test_generated_050_xml() {
+    let xml = include_str!("sicd_meta_050.xml");
+    let sicd_meta = from_str::<sicd_rs::dep::v0_5_0::SicdMeta>(xml);
+    assert!(match sicd_meta {
+        Ok(_) => true,
+        Err(err) => {
+            dbg!(err);
+            false
+        }
+    });
+}
+
+macro_rules! create_component_test {
+    ($($x:ident),*) => {
+#[test]
+fn parse_components_test_macro() {
     let xml = include_str!("sicd_meta_130.xml");
     let mut reader = Reader::from_str(xml);
     reader.trim_text(true);
@@ -28,40 +42,53 @@ fn parse_components_test() {
         match reader.read_event() {
             Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
             Ok(Event::Eof) => break,
-            Ok(Event::Start(e)) => match e.name().as_ref() {
-                b"CollectionInfo" => {
-                    println!("Found CollectionInfo");
-                    let span = reader.read_to_end(QName(b"CollectionInfo")).unwrap();
-                    match from_str::<sicd_rs::v1_3_0::CollectionInfo>(&format!("<CollectionInfo>{}</CollectionInfo>", &xml[span.clone()])) {
-                        Ok(_) => {
-                            println!("Parsed CollectionInfo");
-                        }
+            Ok(Event::Start(e)) => match std::str::from_utf8(e.name().as_ref()).unwrap() {
+                $(
+                stringify!($x) => {
+                    let qname = QName(stringify!($x).as_bytes());
+                    let span = reader.read_to_end(qname).unwrap();
+                    let result = from_str::<sicd_rs::v1_3_0::$x>(&format!(
+                        "<{0}>{1}</{0}>",
+                        stringify!($x),
+                        &xml[span]
+                    ));
+                    match result {
+                        Ok(_) => println!("Parsed {}", stringify!($x)),
                         Err(err) => {
-                            dbg!(&xml[span]);
-                            dbg!(err);
+                            panic!("{} did not parse: {}", stringify!($x), err);
                         }
                     }
                 }
-                b"Antenna" => {
-                    println!("Found antenna");
-                    let span = reader.read_to_end(QName(b"Antenna")).unwrap();
-                    match from_str::<sicd_rs::v1_3_0::Antenna>(&format!("<Antenna>{}</Antenna>", &xml[span.clone()])) {
-                        Ok(_) => {
-                            println!("Parsed Antenna");
-                        }
-                        Err(err) => {
-                            dbg!(err);
-                        }
-                    }
-                }
+            )*
                 other => {
                     if print_count < 3 {
                         print_count += 1;
-                        println!("Found other tag {}", std::str::from_utf8(other).unwrap());
+                        println!("Found other tag {}", other);
                     }
                 }
             },
             _ => (),
         }
     }
+        }
+
+    };
 }
+
+create_component_test!(
+    CollectionInfo,
+    ImageCreation,
+    ImageData,
+    GeoData,
+    Grid,
+    Timeline,
+    Position,
+    RadarCollection,
+    ImageFormation,
+    SCPCOA,
+    Radiometric,
+    Antenna,
+    ErrorStatistics,
+    MatchInfo,
+    PFA
+);
