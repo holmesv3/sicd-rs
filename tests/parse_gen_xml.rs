74738a994cb29bf9783bdd6f92ae24a625bf85fa
Jeff Pursell <jpursell@gmail.com>
2023-12-01 13:03:07 -0500

    Add generated 040 tests

--- tests/parse_gen_xml.rs
+++ tests/parse_gen_xml.rs
@@ -29,6 +29,19 @@ fn test_generated_050_xml() {
     });
 }
 
+#[test]
+fn test_generated_040_xml() {
+    let xml = include_str!("sicd_meta_040.xml");
+    let sicd_meta = from_str::<sicd_rs::dep::v0_4_0::SicdMeta>(xml);
+    assert!(match sicd_meta {
+        Ok(_) => true,
+        Err(err) => {
+            dbg!(err);
+            false
+        }
+    });
+}
+
 macro_rules! create_130_component_test {
     ($($x:ident),*) => {
 #[test]
@@ -172,5 +185,81 @@ create_050_component_test!(
     Antenna,
     ErrorStatistics,
     MatchInfo,
-    PFA//, RGAZCOMP
+    PFA,
+    RGAZCOMP
+);
+
+macro_rules! create_040_component_test {
+    ($($x:ident),*) => {
+#[test]
+fn parse_040_components_test_macro() {
+    let xml = include_str!("sicd_meta_040.xml");
+    let mut reader = Reader::from_str(xml);
+    reader.trim_text(true);
+
+    let mut print_count = 0;
+    loop {
+        match reader.read_event() {
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            Ok(Event::Eof) => break,
+            Ok(Event::Start(e)) => match std::str::from_utf8(e.name().as_ref()).unwrap() {
+                $(
+                stringify!($x) => {
+                    let attributes = e.attributes().map( |a| {
+                        let a=a.unwrap();
+                        format!(
+                            "{}=\"{}\"",
+                            std::str::from_utf8(a.key.into_inner()).unwrap(),
+                            std::str::from_utf8(&a.value).unwrap())}).collect::<Vec<_>>().join(" ");
+                    let qname = QName(stringify!($x).as_bytes());
+                    let span = reader.read_to_end(qname).unwrap();
+                    let xml_sub = format!(
+                        "<{0} {2}>{1}</{0}>",
+                        stringify!($x),
+                        &xml[span],
+                        attributes
+                    );
+                    let result = from_str::<sicd_rs::dep::v0_4_0::$x>(&xml_sub);
+                    match result {
+                        Ok(_) => println!("Parsed {}", stringify!($x)),
+                        Err(err) => {
+                            dbg!(xml_sub);
+                            dbg!(attributes);
+                            panic!("{} did not parse: {}", stringify!($x), err);
+                        }
+                    }
+                }
+            )*
+                other => {
+                    if print_count < 3 {
+                        print_count += 1;
+                        println!("Found other tag {}", other);
+                    }
+                }
+            },
+            _ => (),
+        }
+    }
+        }
+
+    };
+}
+
+create_040_component_test!(
+    CollectionInfo,
+    ImageCreation,
+    ImageData,
+    GeoData,
+    Grid,
+    Timeline,
+    Position,
+    RadarCollection,
+    ImageFormation,
+    SCPCOA,
+    Radiometric,
+    Antenna,
+    ErrorStatistics,
+    MatchInfo,
+    PFA,
+    RGAZCOMP
 );
