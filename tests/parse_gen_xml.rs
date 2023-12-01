9a653c89b5284f6fd05110e57f4fbd5b40d162f3
Jeff Pursell <jpursell@gmail.com>
2023-12-01 09:36:18 -0500

    Add 050 component test

--- tests/parse_gen_xml.rs
+++ tests/parse_gen_xml.rs
@@ -29,10 +29,10 @@ fn test_generated_050_xml() {
     });
 }
 
-macro_rules! create_component_test {
+macro_rules! create_130_component_test {
     ($($x:ident),*) => {
 #[test]
-fn parse_components_test_macro() {
+fn parse_130_components_test_macro() {
     let xml = include_str!("sicd_meta_130.xml");
     let mut reader = Reader::from_str(xml);
     reader.trim_text(true);
@@ -75,7 +75,71 @@ fn parse_components_test_macro() {
     };
 }
 
-create_component_test!(
+create_130_component_test!(
+    CollectionInfo,
+    ImageCreation,
+    ImageData,
+    GeoData,
+    Grid,
+    Timeline,
+    Position,
+    RadarCollection,
+    ImageFormation,
+    SCPCOA,
+    Radiometric,
+    Antenna,
+    ErrorStatistics,
+    MatchInfo,
+    PFA
+);
+
+macro_rules! create_050_component_test {
+    ($($x:ident),*) => {
+#[test]
+fn parse_050_components_test_macro() {
+    let xml = include_str!("sicd_meta_050.xml");
+    let mut reader = Reader::from_str(xml);
+    reader.trim_text(true);
+
+    let mut print_count = 0;
+    loop {
+        match reader.read_event() {
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            Ok(Event::Eof) => break,
+            Ok(Event::Start(e)) => match std::str::from_utf8(e.name().as_ref()).unwrap() {
+                $(
+                stringify!($x) => {
+                    let qname = QName(stringify!($x).as_bytes());
+                    let span = reader.read_to_end(qname).unwrap();
+                    let result = from_str::<sicd_rs::dep::v0_5_0::$x>(&format!(
+                        "<{0}>{1}</{0}>",
+                        stringify!($x),
+                        &xml[span]
+                    ));
+                    match result {
+                        Ok(_) => println!("Parsed {}", stringify!($x)),
+                        Err(err) => {
+                            panic!("{} did not parse: {}", stringify!($x), err);
+                        }
+                    }
+                }
+            )*
+                other => {
+                    if print_count < 3 {
+                        print_count += 1;
+                        println!("Found other tag {}", other);
+                    }
+                }
+            },
+            _ => (),
+        }
+    }
+        }
+
+    };
+}
+
+create_050_component_test!(
     CollectionInfo,
     ImageCreation,
     ImageData,
